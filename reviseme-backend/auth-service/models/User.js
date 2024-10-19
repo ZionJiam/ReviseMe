d978f1dc54dce360e4bb6973e45317b5ced63432
Zion Jiam <zionjiam88.18@gmail.com>
2024-10-20 02:47:08 +0800

    Revert auth-service code

--- reviseme-backend/auth-service/models/User.js
+++ reviseme-backend/auth-service/models/User.js
@@ -1,11 +1,22 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
+// const bcrypt = require('bcryptjs');
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
-});
+// const userSchema = new mongoose.Schema({
+//   googleId: { type: String, unique: true, sparse: true },
+//   email: { type: String, required: true, unique: true },
+//   password: { type: String },  // Not required because Google OAuth users won't have passwords
+//   name: { type: String, required: true }
+// });
 
-const User = mongoose.model('User', userSchema);
+// // Password hashing middleware for local users
+// userSchema.pre('save', function(next) {
+//   if (this.isModified('password')) {
+//     const salt = bcrypt.genSaltSync(10);
+//     this.password = bcrypt.hashSync(this.password, salt);
+//   }
+//   next();
+// });
 
-module.exports = User;
+// const User = mongoose.model('User', userSchema);
+
+// module.exports = User;
