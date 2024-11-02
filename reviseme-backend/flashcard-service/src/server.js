5f577e09759e66afecbf0dcf3c26a4311572a6f4
23xy23 <125449392+23xy23@users.noreply.github.com>
2024-11-02 19:51:55 +0800

    Update .env

--- reviseme-backend/flashcard-service/src/server.js
+++ reviseme-backend/flashcard-service/src/server.js
@@ -10,7 +10,7 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerJsDoc = require('swagger-jsdoc');
 
 const server = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5001;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/flashcards';
 
 const options = {
@@ -23,7 +23,7 @@ const options = {
         },
         servers: [
             {
-                url: 'http://localhost:3000',
+                url: 'http://localhost:5001',
             },
         ],
     },
